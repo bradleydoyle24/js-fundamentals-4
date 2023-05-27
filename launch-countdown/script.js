e6386f0e929743d7ab9c452eed15db1457fe6913
Bradley <bradleydoyle24@gmail.com>
2023-05-27 17:42:22 -0400

    Modified files to mimic online exercise

---
+++ launch-countdown/script.js
@@ -0,0 +1,22 @@
+let output = document.querySelector('.output');
+output.innerHTML = '';
+
+// let i = 10;
+
+// const para = document.createElement('p');
+// para.textContent = ;
+// output.appendChild(para);
+
+
+//start at 10
+// for 10, print Countdown 10
+//iterate through numbers, printing each one to screen
+
+//then , when reaching 0, instead print:
+//  blast off!
+
+const para = document.createElement('p');
+para.textContent = "test";
+output.appendChild(para);
+
+
